cb059080b6d7e0d9435cda1b958044da7e01cc60
Michael Fiano <michael.fiano@gmail.com>
2016-03-12 18:47:22 -0500

    minor changes to neighborhoods

--- neighbors.lisp
+++ neighbors.lisp
@@ -1,6 +1,7 @@
 (in-package :crawler2)
 
-(defstruct (neighborhood (:conc-name nil))
+(defstruct (neighborhood (:conc-name nil)
+                         (:constructor %make-neighborhood))
   stage
   x
   y
@@ -55,96 +56,95 @@
 (defun ne (neighborhood &optional (distance 1))
   (nref neighborhood distance distance))
 
-(defun generate-neighborhood (set-fn map-fn extent-args)
+(defun make-neighborhood (set-fn map-fn extent-args)
   (lambda (stage x y)
-    (make-neighborhood :stage stage
-                       :x x
-                       :y y
-                       :extent (apply #'make-extent extent-args)
-                       :set-fn set-fn
-                       :map-fn map-fn)))
+    (%make-neighborhood :stage stage
+                        :x x
+                        :y y
+                        :extent (apply #'make-extent extent-args)
+                        :set-fn set-fn
+                        :map-fn map-fn)))
 
-(defmethod neighborhood ((layout (eql :ortho)) &rest extent-args)
-  (generate-neighborhood #'nset-ortho #'nmap-ortho extent-args))
+(defmethod layout ((name (eql :ortho)) &rest extent-args)
+  (make-neighborhood #'nset-ortho #'nmap-ortho extent-args))
 
-(defmethod neighborhood ((layout (eql :diag)) &rest extent-args)
-  (generate-neighborhood #'nset-diag #'nmap-diag extent-args))
+(defmethod layout ((name (eql :diag)) &rest extent-args)
+  (make-neighborhood #'nset-diag #'nmap-diag extent-args))
 
-(defmethod neighborhood ((layout (eql :circle)) &rest extent-args)
-  (generate-neighborhood #'nset-circle #'nmap-default extent-args))
+(defmethod layout ((name (eql :circle)) &rest extent-args)
+  (make-neighborhood #'nset-circle #'nmap-default extent-args))
 
-(defmethod neighborhood ((layout (eql :circle-outline)) &rest extent-args)
-  (generate-neighborhood #'nset-circle-outline #'nmap-default extent-args))
+(defmethod layout ((name (eql :circle-outline)) &rest extent-args)
+  (make-neighborhood #'nset-circle-outline #'nmap-default extent-args))
 
-(defmethod neighborhood ((layout (eql :circle-outline+origin)) &rest extent-args)
-  (generate-neighborhood #'nset-circle-outline+origin #'nmap-default extent-args))
+(defmethod layout ((name (eql :circle-outline+origin)) &rest extent-args)
+  (make-neighborhood #'nset-circle-outline+origin #'nmap-default extent-args))
 
-(defmethod neighborhood ((layout (eql :square)) &rest extent-args)
-  (generate-neighborhood #'nset-default #'nmap-default extent-args))
+(defmethod layout ((name (eql :square)) &rest extent-args)
+  (make-neighborhood #'nset-square #'nmap-square extent-args))
 
-(defmethod neighborhood ((layout (eql :square-outline)) &rest extent-args)
-  (generate-neighborhood #'nset-square-outline #'nmap-square-outline extent-args))
+(defmethod layout ((name (eql :square-outline)) &rest extent-args)
+  (make-neighborhood #'nset-square-outline #'nmap-square-outline extent-args))
 
-(defmethod neighborhood ((layout (eql :square-outline+origin)) &rest extent-args)
-  (generate-neighborhood #'nset-square-outline+origin #'nmap-square-outline+origin extent-args))
+(defmethod layout ((layout (eql :square-outline+origin)) &rest extent-args)
+  (make-neighborhood #'nset-square-outline+origin #'nmap-square-outline+origin extent-args))
 
 (defun nset-ortho (neighborhood x y)
-  (with-slots (max) (extent neighborhood)
-    (and (<= (abs x) max)
-         (<= (abs y) max)
+  (with-slots (maximum) (extent neighborhood)
+    (and (<= (abs x) maximum)
+         (<= (abs y) maximum)
          (or (and (zerop x) (zerop y))
              (and (zerop x) (not (zerop y)))
              (and (not (zerop x)) (zerop y))))))
 
 (defun nmap-ortho (neighborhood func)
-  (with-slots (max) (extent neighborhood)
+  (with-slots (maximum) (extent neighborhood)
     (let ((results))
-      (loop :for y :from (- max) :to max
+      (loop :for y :from (- maximum) :to maximum
             :for cell = (nref neighborhood 0 y)
             :when cell
               :collect (funcall func cell) :into results)
-      (loop :for x :from (- max) :below 0
+      (loop :for x :from (- maximum) :below 0
             :for cell = (nref neighborhood x 0)
             :when cell
               :collect (funcall func cell) :into results)
-      (loop :for x :from 1 :to max
+      (loop :for x :from 1 :to maximum
             :for cell = (nref neighborhood x 0)
             :when cell
               :collect (funcall func cell) :into results)
       results)))
 
 (defun nset-diag (neighborhood x y)
-  (with-slots (max) (extent neighborhood)
-    (and (<= (abs x) max)
-         (<= (abs y) max)
+  (with-slots (maximum) (extent neighborhood)
+    (and (<= (abs x) maximum)
+         (<= (abs y) maximum)
          (= (abs x) (abs y)))))
 
 (defun nmap-diag (neighborhood func)
-  (with-slots (max) (extent neighborhood)
+  (with-slots (maximum) (extent neighborhood)
     (let ((results))
-      (loop :for x :from (- max) :to max
+      (loop :for x :from (- maximum) :to maximum
             :for cell = (nref neighborhood x (- x))
             :when cell
               :collect (funcall func cell) :into results)
-      (loop :for x :from (- max) :below 0
+      (loop :for x :from (- maximum) :below 0
             :for cell = (nref neighborhood x x)
             :when cell
               :collect (funcall func cell) :into results)
-      (loop :for x :from 1 :to max
+      (loop :for x :from 1 :to maximum
             :for cell = (nref neighborhood x x)
             :when cell
               :collect (funcall func cell) :into results)
       results)))
 
 (defun nset-circle (neighborhood x y)
-  (with-slots (max) (extent neighborhood)
+  (with-slots (maximum) (extent neighborhood)
     (<= (+ (* x x) (* y y))
-        (* max max))))
+        (* maximum maximum))))
 
 (defun nset-circle-outline (neighborhood x y)
   (with-slots (minimum maximum) (extent neighborhood)
-    (and (<= (+ (* x x) (* y y))
-             (* maximum maximum))
+    (and (nset-circle neighborhood x y)
          (> (+ (* x x) (* y y))
             (* (1- minimum) (1- minimum))))))
 
@@ -153,18 +153,19 @@
       (and (zerop x)
            (zerop y))))
 
+(defun nset-square (neighborhood x y)
+  (nset-default neighborhood x y))
+
+(defun nmap-square (neighborhood func)
+  (nmap-default neighborhood func))
+
 (defun nset-square-outline (neighborhood x y)
   (with-slots (minimum maximum) (extent neighborhood)
-    (and
-     (and (>= x (- maximum))
-          (>= y (- maximum))
-          (<= x maximum)
-          (<= y maximum))
-     (not
-      (and (> x (- minimum))
-           (> y (- minimum))
-           (< x minimum)
-           (< y minimum))))))
+    (and (nset-square neighborhood x y)
+         (not (and (> x (- minimum))
+                   (> y (- minimum))
+                   (< x minimum)
+                   (< y minimum))))))
 
 (defun nmap-square-outline (neighborhood func)
   (with-slots (minimum maximum) (extent neighborhood)
